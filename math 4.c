142b33e58086f3c7e8ba0b1cccdd68f7e615050c
Ronak kumar <72099466+Ronakkumar-dev@users.noreply.github.com>
2020-10-01 02:29:06 +0530

    Update math 4.c

--- math 4.c
+++ math 4.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    int a,b,,s;
+    int a,b,c,s;
     flot avg,d;
     printf("\ntype 3 number:- ");
-    scanf("%d%d%d",&a,,&c);
+    scanf("%d%d%d",&a,&b,&c);
     s=a+b+c;
     avg=s/3;
     d=(s-avg);
-    pritf("\n Difference=",d);
+    pritf("\n Difference=%f",d);
     return 0;
 }
