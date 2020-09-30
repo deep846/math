a9fb387509c112123bda6d60074a2b0b3db23df1
deep846 <45318452+deep846@users.noreply.github.com>
2020-10-01 02:06:37 +0530

    math file uploded

---
+++ math 4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#incde <conio.h>
+
+int main()
+{
+    int a,b,,s;
+    flot avg,d;
+    printf("\ntype 3 number:- ");
+    scanf("%d%d%d",&a,,&c);
+    s=a+b+c;
+    avg=s/3;
+    d=(s-avg);
+    pritf("\n Difference=",d);
+    return 0;
+}
